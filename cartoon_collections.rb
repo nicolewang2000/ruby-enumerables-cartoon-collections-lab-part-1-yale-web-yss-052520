70a8706b44f28715603cdcace37f9b869d37366b
Nicole Wang <nicole.wang@yale.edu>
2020-05-12 00:09:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each 
+  array.each do |element|
+    Hello 
+  end 
 end
 
 def list_dwarves(array)
